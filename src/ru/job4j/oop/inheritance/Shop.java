98021deeb79f6986e2099e8b686c3dea340a8eb3
Konstantin Podgorny <podgorny.kv@gmail.com>
2021-01-31 08:33:12 +0300

    6. Почему наследование - это плохо [#446673] v2

--- src/ru/job4j/oop/inheritance/Shop.java
+++ src/ru/job4j/oop/inheritance/Shop.java
@@ -2,13 +2,13 @@ package ru.job4j.oop.inheritance;
 
 public class Shop {
     public static void main(String[] args) {
-        Product product = (Product) of("Oil", 100);
+        Product product = of("Oil", 100);
         System.out.println(product.label());
     }
 
-    public static Object of(String name, int price) {
+    public static Product of(String name, int price) {
         if ("Oil".equals(name)) {
-            return new LiquidationProduct(name, price);
+            return new Product(name, price);
         }
         return new Product(name, price);
     }
