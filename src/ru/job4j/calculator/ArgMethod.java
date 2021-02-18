f6578fcb09f07002a6a83810387cdbe666ce0c6e
Konstantin Podgorny <podgorny.kv@gmail.com>
2021-02-18 12:32:56 +0300

    remove static

--- src/ru/job4j/calculator/ArgMethod.java
+++ src/ru/job4j/calculator/ArgMethod.java
@@ -9,9 +9,8 @@ public class ArgMethod {
     public static void main(String[] args) {
         String name = "Petr Arsentev";
         int age = 33;
-        ArgMethod.hello(name);
-        ArgMethod.hello(name);
-        ArgMethod.hello(name);
-        ArgMethod.hello(name);
+        hello(name);
+        hello(name);
+        hello(name);
     }
 }
