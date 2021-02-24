ba1abfa0c6bf78a9807b6af21a5d92501c329fc6
Konstantin Podgorny <podgorny.kv@gmail.com>
2021-02-24 22:58:26 +0300

    1. Телефонный справочник на базе ArrayList [#446586]

---
+++ test/ru/job4j/collection/search/PhoneDictionaryTest.java
@@ -0,0 +1,21 @@
+package ru.job4j.collection.search;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class PhoneDictionaryTest {
+
+    @Test
+    public void whenFindByName() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("Petr");
+        assertThat(persons.get(0).getSurname(), is("Arsentev"));
+    }
+}
