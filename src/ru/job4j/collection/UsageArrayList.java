e34eea6d5c9d77715fa74bf543f262d35fb44490
Konstantin Podgorny <podgorny.kv@gmail.com>
2021-02-24 22:35:20 +0300

    0. Коллекции, ArrayList, List, Обобщения [#446592]

---
+++ src/ru/job4j/collection/UsageArrayList.java
@@ -0,0 +1,17 @@
+package ru.job4j.collection;
+
+import java.util.ArrayList;
+
+public class UsageArrayList {
+
+    public static void main(String[] args) {
+        ArrayList<String> arr = new ArrayList<>();
+        arr.add("Petr");
+        arr.add("Ivan");
+        arr.add("Stepan");
+
+        for (String str : arr) {
+            System.out.println(str);
+        }
+    }
+}
