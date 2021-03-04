d2f21a1b41501ad619e3d5d16d17413b340558e8
Konstantin Podgorny <podgorny.kv@gmail.com>
2021-03-04 23:21:35 +0300

    0. Map, HashMap [#446594]

---
+++ src/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,17 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UsageMap {
+
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("lolo@gmail.com", "Tapkov Tapok Tapkovich");
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            String key = entry.getKey();
+            String value = entry.getValue();
+            System.out.println(key + " = " + value);
+        }
+    }
+}
