e72171e65ff2cbf94d77d7ffebb7c68261476d88
Konstantin Podgorny <podgorny.kv@gmail.com>
2021-03-09 22:19:20 +0300

    2. Комбинированный компаратор [#446583]

--- src/ru/job4j/collection/JobSorter.java
+++ src/ru/job4j/collection/JobSorter.java
@@ -2,21 +2,34 @@ package ru.job4j.collection;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class JobSorter {
 
     public static void main(String[] args) {
         List<Job> jobs = Arrays.asList(
-                new Job("Fix bugs", 4),
-                new Job("Impl task", 2),
-                new Job("Reboot server", 1)
+                new Job("Fix bug", 1),
+                new Job("Fix bug", 4),
+                new Job("Fix bug", 2),
+                new Job("X task", 0)
         );
         System.out.println(jobs);
+
         Collections.sort(jobs);
         System.out.println(jobs);
+
         Collections.sort(jobs, new SortByNameJob());
         //jobs.sort(new SortByNameJob());
+        System.out.println(jobs);
+
+        Collections.sort(jobs, new JobDescByName().thenComparing(new JobDescByPriority()));
+        System.out.println(jobs);
+
+        Comparator<Job> comb = new JobDescByNameLn()
+                .thenComparing(new JobDescByName())
+                .thenComparing(new JobDescByPriority());
+        Collections.sort(jobs, comb);
         System.out.println(jobs);
     }
 }
---
+++ test/ru/job4j/collection/JobTest.java
@@ -0,0 +1,102 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import java.util.Comparator;
+
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.lessThan;
+import static org.junit.Assert.assertThat;
+
+public class JobTest {
+
+    @Test
+    public void whenCompatorByNameAndPrority() {
+        Comparator<Job> cmpNamePriority = new JobDescByName().thenComparing(new JobDescByPriority());
+        int rsl = cmpNamePriority.compare(
+                new Job("Impl task", 0),
+                new Job("Fix bug", 1)
+        );
+        assertThat(rsl, lessThan(0));
+    }
+
+    @Test
+    public void whenComparatorByDescNameAndAscPriority() {
+        Comparator<Job> cmpNamePriority = new JobDescByName().thenComparing(new JobAscByPriority());
+        int rsl = cmpNamePriority.compare(
+                new Job("Impl task", 0),
+                new Job("Impl task", 1)
+        );
+        assertThat(rsl, lessThan(0));
+    }
+
+    @Test
+    public void whenComparatorByAscNameAndDescPriority() {
+        Comparator<Job> cmpNamePriority = new JobAscByName().thenComparing(new JobDescByPriority());
+        int rsl = cmpNamePriority.compare(
+                new Job("Impl task", 0),
+                new Job("Impl task", 1)
+        );
+        assertThat(rsl, greaterThan(0));
+    }
+
+    @Test
+    public void whenComparatorByAscPriorityAndDescName() {
+        Comparator<Job> cmpPriorityName = new JobAscByPriority().thenComparing(new JobDescByName());
+        int rsl = cmpPriorityName.compare(
+                new Job("Impl task", 0),
+                new Job("Fix bug", 1)
+        );
+        assertThat(rsl, lessThan(0));
+    }
+
+    @Test
+    public void whenComparatorByDescPriorityAndAscName() {
+        Comparator<Job> cmpPriorityName = new JobDescByPriority().thenComparing(new JobAscByName());
+        int rsl = cmpPriorityName.compare(
+                new Job("Impl task", 0),
+                new Job("Fix bug", 1)
+        );
+        assertThat(rsl, greaterThan(0));
+    }
+
+    @Test
+    public void whenComparatorAscByName() {
+        Comparator<Job> cmpAscByName = new JobAscByName();
+        int rsl = cmpAscByName.compare(
+                new Job("Impl task", 0),
+                new Job("Fix bug", 1)
+        );
+        assertThat(rsl, greaterThan(0));
+    }
+
+    @Test
+    public void whenComparatorDescByName() {
+        Comparator<Job> cmpDescByName = new JobDescByName();
+        int rsl = cmpDescByName.compare(
+                new Job("Impl task", 0),
+                new Job("Fix bug", 1)
+        );
+        assertThat(rsl, lessThan(0));
+    }
+
+    @Test
+    public void whenComparatorAscByPriority() {
+        Comparator<Job> cmpAscByPriority = new JobAscByPriority();
+        int rsl = cmpAscByPriority.compare(
+                new Job("Impl task", 0),
+                new Job("Fix bug", 1)
+        );
+        assertThat(rsl, lessThan(0));
+    }
+
+    @Test
+    public void whenComparatorDescByPriority() {
+        Comparator<Job> cmpDescByPriority = new JobDescByPriority();
+        int rsl = cmpDescByPriority.compare(
+                new Job("Impl task", 0),
+                new Job("Fix bug", 1)
+        );
+        assertThat(rsl, greaterThan(0));
+    }
+}
