8a34bad2c0b76ac638ba4ee5fa03cd25c14efd0b
Konstantin Podgorny <podgorny.kv@gmail.com>
2021-04-04 19:59:04 +0300

    0. Stream API [#446418]

---
+++ src/ru/job4j/collection/StreamApi.java
@@ -0,0 +1,19 @@
+package ru.job4j.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamApi {
+    public static void main(String[] args) {
+        ArrayList<Integer> num = new ArrayList<>();
+        num.add(-1);
+        num.add(2);
+        num.add(-3);
+        num.add(4);
+
+        List<Integer> posNum = num.stream()
+                .filter(n -> n % 2 == 0)
+                .collect(Collectors.toList());
+    }
+}
