aafd5c2c3a5aa6034f4e4a54ac17afda40aa6f6d
Konstantin Podgorny <podgorny.kv@gmail.com>
2023-12-29 00:01:14 +0400

    6. Правила Checkstyle [#504981] _wrong variant

---
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -0,0 +1,30 @@
+package ru.job4j.checkstyle;
+
+public class Broken {
+    private int size_of_empty=10;
+
+    public String Surname ;
+
+
+    public static final String newValue = "";
+
+    void echo() { }
+
+
+    void Media (Object obj) {
+        if (obj != null)
+            System.out.println( obj );
+    }
+
+
+
+    void method(int a, int b, int c, int d, int e, int f, int g, int h)
+    {
+
+    }
+
+
+    String Name;
+
+    Broken() {}
+}
