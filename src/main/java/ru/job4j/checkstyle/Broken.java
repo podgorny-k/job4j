7da95b5c7556d889e16b2564a89651257575a7b8
Konstantin Podgorny <podgorny.kv@gmail.com>
2023-12-29 00:14:37 +0400

    6. Правила Checkstyle [#504981]

--- src/main/java/ru/job4j/checkstyle/Broken.java
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -1,30 +1,23 @@
 package ru.job4j.checkstyle;
 
 public class Broken {
-    private int size_of_empty=10;
+    public static final String NEW_VALUE = "";
+    public String surname;
+    String name;
+    private int sizeOfEmpty = 10;
 
-    public String Surname ;
-
-
-    public static final String newValue = "";
-
-    void echo() { }
-
-
-    void Media (Object obj) {
-        if (obj != null)
-            System.out.println( obj );
+    Broken() {
     }
 
-
-
-    void method(int a, int b, int c, int d, int e, int f, int g, int h)
-    {
-
+    void echo() {
     }
 
+    void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
 
-    String Name;
-
-    Broken() {}
+    void method(int a, int b, int c, int d, int e, int f, int g) {
+    }
 }
