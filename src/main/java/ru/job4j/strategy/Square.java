01f2995bd646078ea084ad2ecbe97659f8017213
Konstantin Podgorny <podgorny.kv@gmail.com>
2024-01-05 17:16:03 +0400

    11.0. Шаблон проектирования - Стратегия [#786 #101732] edit

--- src/main/java/ru/job4j/strategy/Square.java
+++ src/main/java/ru/job4j/strategy/Square.java
@@ -9,8 +9,4 @@ public class Square implements Shape {
                 + "|      |" + ln
                 + "|______|" + ln;
     }
-
-    public static void main(String[] args) {
-        System.out.println(new Square().draw());
-    }
 }
