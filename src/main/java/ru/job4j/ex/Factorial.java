d00713098417a1281457cfac1e41f3860fbd9cde
Konstantin Podgorny <podgorny.kv@gmail.com>
2024-01-05 19:59:41 +0400

    0.3. Тестирование исключений с junit [#219366 #101741]

---
+++ src/main/java/ru/job4j/ex/Factorial.java
@@ -0,0 +1,14 @@
+package ru.job4j.ex;
+
+public class Factorial {
+    public static int calc(int number) {
+        if (number < 0) {
+            throw new IllegalArgumentException("Number could not be less than 0");
+        }
+        int result = 1;
+        for (int index = 2; index <= number; index++) {
+            result *= index;
+        }
+        return result;
+    }
+}
